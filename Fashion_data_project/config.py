e925c52b16ad9219fbf9433d795ac7e4528a62bd
change0z <shaikh.ziyad.mr@gmail.com>
2023-11-20 15:14:53 +0000

    Fashion Start

---
+++ Fashion_data_project/config.py
@@ -0,0 +1,33 @@
+class Config:
+   #Image configuration
+   IMG_SIZE = 32
+   PATCH_SIZE = 10
+   CROP_SIZE = 100
+   BATCH_SIZE = 1
+   DATASET_SAMPLE = 'full'
+
+   #opimizer configuration
+   LR = 0.003
+   OPIMIZER = 'Adam'
+
+   #Model configuration
+   NUM_CLASSES = 0
+   IN_CHANNELS = 0
+   HIDDEN_SIZE = 0
+   NUM_ATTENTION_HEADS = 0
+   LINEAR_DIM = 0
+   NUM_LAYERS = 0
+
+   ATTENTION_DROPOUT_RATE = 0.1
+   DROPOUT_RATE = 0.1
+   STD_NORM = 1e-6
+   EPS = 1e-6
+   MPL_DIM = 128
+   OUTPUT = 'softmax'
+   LOSS_FN = 'nll_loss'
+
+   #Device configuration
+   DEVICE = ["cpu","mps","cuda"]
+
+   #Training configuration
+   N_EPOCHS = 1
